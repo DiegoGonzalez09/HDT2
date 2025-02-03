6f2cefa39c83c3625cf7573c5856d30be24d4c55
Ariel Aceytuno <164232812+silversitoooo@users.noreply.github.com>
2025-02-02 19:26:25 -0600

    Update CalculatorException.java

--- src/main/java/CalculatorException.java
+++ src/main/java/CalculatorException.java
@@ -1,1 +1,12 @@
-
+/**
+ * Excepción personalizada para errores de la calculadora.
+ */
+public class CalculatorException extends Exception {
+    /**
+     * Constructor que acepta un mensaje de error.
+     * @param message El mensaje describiendo el error
+     */
+    public CalculatorException(String message) {
+        super(message);
+    }
+}
