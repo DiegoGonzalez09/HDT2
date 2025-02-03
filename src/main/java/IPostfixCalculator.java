b86b89229c5d403d40b9a3a24822b6b1903ab826
Ariel Aceytuno <164232812+silversitoooo@users.noreply.github.com>
2025-02-02 19:27:19 -0600

    Update IPostfixCalculator.java

--- src/main/java/IPostfixCalculator.java
+++ src/main/java/IPostfixCalculator.java
@@ -1,1 +1,13 @@
-
+/**
+ * Interfaz para la calculadora de expresiones postfix.
+ * Define el método principal para evaluar expresiones matemáticas en notación postfija.
+ */
+public interface IPostfixCalculator {
+    /**
+     * Evalúa una expresión matemática en notación postfija.
+     * @param expression La expresión a evaluar en formato String
+     * @return El resultado de la evaluación
+     * @throws CalculatorException Si hay un error en el formato o cálculo de la expresión
+     */
+    int evaluateExpression(String expression) throws CalculatorException;
+}
